cb64c4b9e30af2b6a54f6f1d60daf89bcfb163a7
minhnx8 <minhnx8@fsoft.com.vn_>
2023-05-17 00:43:27 +0700

    add commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres1
+export POSTGRES_PASSWORD=admin1234
+export POSTGRES_HOST=cdr.c7jcn5a69uop.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=mypostgres
+export AWS_BUCKET=project-aws3
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
